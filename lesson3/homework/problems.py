7467b494c7fef68634405b9b40f169bab3f92b2d
Bavnoor. S. S <sokhal.bavnoor@gmail.com>
2026-02-05 21:24:28 -0800

    FInished lesson3 hwk

--- lesson3/homework/problems.py
+++ lesson3/homework/problems.py
@@ -1,12 +1,19 @@
 # Problem 1
 # Ask the user for a number n.
 # Print all multiples of 3 from 0 to n (including n if it is a multiple of 3).
+n:int = int(input("Gimme an integer: "))
 
+for i in range(0, n+1, 3):
+    print(i)
 
 
 # Problem 2
 # Ask the user for a number n.
 # Print the square of every number from 1 to n (1*1, 2*2, ...).
+n = int(input("Gimme another integer: "))
+
+for i in range(1, n+1):
+    print("{}^2 = {}".format(i, i**2))
 
 
 
@@ -14,6 +21,11 @@
 # Ask the user for a number n.
 # Print the numbers from n down to 0 (counting down).
 
+n = int(input("Gimme one more integer: "))
+
+for i in range(n, -1, -1):
+    print("{}...".format(i))
+print("Blast off!")
 
 
 # Problem 4
@@ -21,8 +33,28 @@
 # Build a new string that repeats the word 5 times with spaces in between.
 # Example: "hi hi hi hi hi"
 
+word:str = input("Gimme a word: ")
+nword:str = str()
 
+for i in range(5):
+    nword += word+" "
+
+nword = nword.rstrip()
+
+print(nword)
+
+# or
+
+wordle:str = input("(ENCHANCED: NOW SIMPLER) gimme a word: ")
+nwordle:str = (wordle + " ") * 5
+nwordle = nwordle.rstrip()
+print(nwordle)
 
 # Problem 5
 # Ask the user for a number n.
 # Print how many numbers from 1 to n are even.
+
+n = int(input("Gimme an integer (AGAIN!!): "))
+iter:list[int] = range(1, n+1, 2)
+
+print("There are {} even numbers from 1 to {}".format(len(iter), n))
